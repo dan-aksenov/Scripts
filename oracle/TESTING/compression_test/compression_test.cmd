4981932aab9cf615bd8142d32154c37e7a001576
dan-aksenov <dan-aksenov@yandex.ru>
2016-01-29 15:52:17 +0300

    compression_test

--- oracle/TESTING/compression_test/compression_test.cmd
+++ oracle/TESTING/compression_test/compression_test.cmd
@@ -1,2 +1,2 @@
 sqlplus dbax/dbax@IOTA1 @compression_test %1 %2 %3
-findstr "Testing Elapsed Select Insert Delete Update Size" log\%1_%2.log > log\%1_%2.fin
+findstr "Testing Elapsed Select Insert Delete Update Size" log\%1_%2_%3.log > log\%1_%2_%3.fin
--- oracle/TESTING/compression_test/compression_test.sql
+++ oracle/TESTING/compression_test/compression_test.sql
@@ -19,17 +19,16 @@ end;
 /
 
 alter index COMP_TEST.&3. rebuild nocompress online;
-spoo log\&1._&2..log
+spoo log\&1._&2._&3..log
 PROMPT Testing compression for User &1. Table &2. Index &3.
-select 'Not compressed index Size(MB):'||sum(bytes)/1024/1024 MB from dba_segments where owner = 'COMP_TEST' and segment_type like 'INDEX%' and segment_name = '&3.';
 set timi on
 --set autotrace on
-PROMPT Not compressed index Insert
+PROMPT Notcompressed index Insert
 insert into COMP_TEST.&2. (select * from &1..&2.);
-PROMPT
 set timi off
 commit;
 exec dbms_stats.gather_table_stats(ownname=>'COMP_TEST', tabname=>'&2.');
+select 'Not compressed index Size(MB):'||sum(bytes)/1024/1024 MB from dba_segments where owner = 'COMP_TEST' and segment_type like 'INDEX%' and segment_name = '&3.';
 set timi on
 PROMPT Not compressed index Select
 select /*+ INDEX(a &3.)*/ count(*) from COMP_TEST.&2. a;
@@ -41,7 +40,6 @@ commit;
 --set autotrace off
 
 alter index COMP_TEST.&3. rebuild compress advanced low online;
-select 'Compressed index Size(MB):'||sum(bytes)/1024/1024 MB from dba_segments where owner = 'COMP_TEST' and segment_type like 'INDEX%' and segment_name = '&3.';
 set timi on
 --set autotrace on
 PROMPT Compressed index Insert
@@ -49,6 +47,7 @@ insert into COMP_TEST.&2. (select * from &1..&2.);
 set timi off
 commit;
 exec dbms_stats.gather_table_stats(ownname=>'COMP_TEST', tabname=>'&2.');
+select 'Compressed index Size(MB):'||sum(bytes)/1024/1024 MB from dba_segments where owner = 'COMP_TEST' and segment_type like 'INDEX%' and segment_name = '&3.';
 set timi on
 PROMPT Compressed index Select
 select /*+ INDEX(a &3.)*/ count(*) from COMP_TEST.&2. a;
@@ -57,7 +56,6 @@ delete from COMP_TEST.&2.;
 set timi off
 commit;
 --set autotrace off
-
 
 drop user COMP_TEST cascade;
 
