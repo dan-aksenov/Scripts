97147f6d99eb66fc338d528ad15ac48817a389ad
dan-aksenov <dan-aksenov@yandex.ru>
2016-02-19 13:30:22 +0300

    ddl compare
    minor fixes

--- oracle/ETL/ddl_compare/scripts/objects.sql
+++ oracle/ETL/ddl_compare/scripts/objects.sql
@@ -1,13 +1,13 @@
 
 prompt --ALL OBJECTS COMPARE
 prompt /* MISSING OBJECTS
-select object_name,object_type from dba_objects@&2. where owner = '&1.' and object_name not like 'SYS%'
+select object_type ||': '|| object_name from dba_objects@&2. where owner = '&1.' and object_name not like 'SYS%'
 minus
-select object_name,object_type from dba_objects@&4. where owner = '&3.' and object_name not like 'SYS%';
+select object_type ||': '|| object_name from dba_objects@&4. where owner = '&3.' and object_name not like 'SYS%';
 prompt */
 
 prompt /* SURPLUS OBJECTS
-select object_name,object_type from dba_objects@&4. where owner = '&3.' and object_name not like 'SYS%'
+select object_type ||': '|| object_name from dba_objects@&4. where owner = '&3.' and object_name not like 'SYS%'
 minus
-select object_name,object_type from dba_objects@&2. where owner = '&1.' and object_name not like 'SYS%';
+select object_type ||': '|| object_name from dba_objects@&2. where owner = '&1.' and object_name not like 'SYS%';
 prompt */
