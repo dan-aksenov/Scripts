c6b01c4d1cc940280899c78103ab6d069df112ad
dan-aksenov <dan-aksenov@yandex.ru>
2016-03-14 09:43:03 +0300

    adrci purge

---
+++ oracle/DEBUG/adrci_purge.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+adrci << EOF
+spool adr_homes.lst
+show homes
+spool off
+EOF
+for home in $(tail -n+2 adr_homes.lst); do
+	adrci <<EOF
+	set home $home
+	PURGE -AGE 000 -TYPE ALERT
+	PURGE -AGE 000 -TYPE INCIDENT
+	PURGE -AGE 000 -TYPE TRACE
+	EOF
+done
+rm adr_homes.lst
