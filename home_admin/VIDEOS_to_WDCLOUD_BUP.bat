6c130bb95ddb49a790a667ee7571181d92bca0f9
dan-aksenov <dan-aksenov@yandex.ru>
2016-04-14 22:11:44 +0300

    videos_bup
    codepage change + new path

--- home_admin/VIDEOS_to_WDCLOUD_BUP.bat
+++ home_admin/VIDEOS_to_WDCLOUD_BUP.bat
@@ -1,13 +1,13 @@
 chcp 1251
 
 set log="d:\TMP"
-set src="j:\Home_video_backup"
+set src="h:\Users\Данила\Videos"
 
 IF NOT EXIST %log% MD %log%
 
-set dst="\\wdmycloud\danila\!BUPS\homevideos\Home_video_backup"
+set dst="\\wdmycloud\danila\!BUPS\homevideos"
 
-robocopy %src% %dst% *.* /s /unilog:%log%\photo_to_Ya.log /z /purge
+robocopy %src% %dst% *.* /s /unilog:%log%\video_to_mycloud.log /z /purge
 
 forfiles -p %log% -s -m *.* /D -10 /C "cmd /c del @path"
  
