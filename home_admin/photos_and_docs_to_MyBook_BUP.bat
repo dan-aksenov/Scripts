5ee195e51825a0bd96a9a8210d69b3940fd10feb
dan-aksenov <dan-aksenov@yandex.ru>
2016-04-17 13:08:28 +0300

    home to wd
    old bup added

--- home_admin/photos_and_docs_to_MyBook_BUP.bat
+++ home_admin/photos_and_docs_to_MyBook_BUP.bat
@@ -2,9 +2,11 @@ chcp 1251
 
 rem todo: add options for incr and full
 
-set log=d:\temp
+set log=d:\tmp
 
 IF NOT EXIST %log% MD %log%
+
+robocopy K:\HOME_BUP K:\HOME_BUP_OLD *.* /e /unilog:"%log%\home_to_old.log" /xd YandexDisk /TEE /z /purge
 
 set src="d:\Users\Данила"
 
