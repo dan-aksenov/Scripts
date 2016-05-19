f9ee2ce9b3d7929a5a521236a1db73cf69451f21
dan-aksenov <dan-aksenov@yandex.ru>
2016-05-19 10:55:06 +0300

    copy_to_smb2
    copy with ifmounted check.

---
+++ linux/copy_to_smb2.sh
@@ -0,0 +1,13 @@
+MOUNT_DIR=$HOME/windows_share
+REMOTE_DIR=//FS/TESK_LKK/
+BACKUP_DIR=$HOME/9.4/backups/
+
+# check if share is mounted
+if mountpoint -q -- $MOUNT_DIR; 
+ then
+  printf '%s\n' "$dir is a mounted"
+ else printf '%s\n' "$dir is a mounted, exiting" && exit 1 
+fi
+
+# Copy backups to remote windows location
+rsync -avz $BACKUP_DIR $MOUNT_DIR/$HOSTNAME
