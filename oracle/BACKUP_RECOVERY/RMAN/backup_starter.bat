281b7c7e12782f6921f8986ae34df9d1330b13f9
dan-aksenov <dan-aksenov@yandex.ru>
2016-05-25 13:45:56 +0300

    backup_starter
    add parameters for paths

--- oracle/BACKUP_RECOVERY/RMAN/backup_starter.bat
+++ oracle/BACKUP_RECOVERY/RMAN/backup_starter.bat
@@ -3,6 +3,9 @@ rem tags: 0,1,arch (for level 0,1 and archivelog backups)
 
 set tag=%1
 
+set OFFSITE=
+set BKP_DIR=
+
 rem send old logs to zip
 set log=%time:~0,2%%time:~3,2%%time:~6,2%_%date:~-10,2%%date:~-7,2%%date:~-4,4%_%tag%.log
 zip -g old_logs.zip *%tag%.log
@@ -11,8 +14,10 @@ del *%tag%.log -y
 rem actual rman backup
 rman target / @backup%tag%.cmd LOG='%log%'
 
-rem backup backups to remote server. (to do: add by parameters)
-net use \\fs1\d$ /user:localhost\dbax dbax
-robocopy c:\backup\rman \\fs1\d$\backup\THETA\ *.* /z /s /MIR /mt /log:.\backup_to_fs.log
-net use \\fs1\d$ /delete
+rem backup backups to remote server.
+
+net use %OFFSITE% /user:localhost\dbax p@S$w0rd
+robocopy %BKP_DIR% %OFFSITE% *.* /z /s /MIR /mt /log:.\remote_backup.log
+net use %OFFSITE% /delete
+
 exit
