a0bf4f8201444b1fd26d6dbb5c81bc4d71d6edd1
dan-aksenov <dan-aksenov@yandex.ru>
2016-05-26 18:28:29 +0300

    pg_backup_my
    windows dumps tweeks

--- postgresql/pg_backup_my.bat
+++ postgresql/pg_backup_my.bat
@@ -1,4 +1,4 @@
-rem Script dumps PG database into file named as db_name_date_time.
+rem Script dumps PG database into file named as db_name_time_date.
 rep password supplied via password file: "Appdata\Roaming\postgresql\pgpass.conf"
 
 rem User supplied variables.
@@ -6,28 +6,35 @@ set hst=localhost
 set prt=5432
 set usr=postgres
 set fmt=c
-set base=TESK_LKK
+set base=%1
 rem Delete files older then keep
 set keep=30
 
+set OFFSITE=
+set BKP_DIR=
+
 rem Variables for timestamp and file names.
 set stamp=%time:~0,2%%time:~3,2%%time:~6,2%_%date:~-10,2%%date:~-7,2%%date:~-4,4%
-set dmp=%base%_%stamp%.backup
-set log=%base%_%stamp%.log
+set dmp="%BKP_DIR%\%base%_%stamp%.backup"
+set log="%BKP_DIR%\%base%_%stamp%.log"
 
 
 rem Delete old files
-forfiles -p . -s -m *.* /D -%keep% /C "cmd /c del @path"
+forfiles -p %BKP_DIR% -s -m *.* /D -%keep% /C "cmd /c del @path"
 
 rem Backup DB
-pg_dump -h %hst% -p %prt% -U "%usr%" -w -F %fmt% -b -v -f "%dmp%" "%base%" 2>"%log%"
+IF NOT EXIST Backup MD Backup
+pg_dump -h %hst% -p %prt% -U "%usr%" -w -F %fmt% -b -v -f %dmp% %base% 2>%log%
 
 rem Error section
 IF NOT %ERRORLEVEL%==0 GOTO Error
 GOTO End
 
-rem :Error
-powershell -file mail.ps1 %log%
-
+:Error
+rem powershell -file mail.ps1 %log%
 
 :End
+rem remote_copy
+net use \\fs\d$ /user:localhost\user password
+robocopy %BKP_DIR% %OFFSITE% *.* /z /s /MIR /mt /log:.\remote_backup.log
+net use \\fs\d$ /delete /yes
