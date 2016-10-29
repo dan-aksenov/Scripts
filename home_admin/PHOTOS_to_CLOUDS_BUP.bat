7c5ead9d022341b7a4dfbf06f42f41078c9abada
dan-aksenov <dan-aksenov@yandex.ru>
2016-10-29 20:57:48 +0300

    home backups
    minor fixes

--- home_admin/PHOTOS_to_CLOUDS_BUP.bat
+++ home_admin/PHOTOS_to_CLOUDS_BUP.bat
@@ -8,10 +8,10 @@ set src="d:\Users\Данила"
 IF NOT EXIST %log% MD %log%
 
 set dst="d:\Users\Данила\YandexDisk\!BUPS"
-start call robocopy %src% %dst% *.* /s /unilog:%log%\photo_to_Ya.log /z /purge /mt
+start call robocopy %src% %dst% *.* /s /unilog:%log%\photo_to_Ya.log /z /purge /mt /xd YandexDisk
 
 set dst="\\wdmycloud\danila\!BUPS"
-start call robocopy %src% %dst% *.* /s /unilog:%log%\photo_to_mycloud.log /z /purge /mt
+start call robocopy %src% %dst% *.* /s /unilog:%log%\photo_to_mycloud.log /z /purge /mt /xd YandexDisk
 
 rem forfiles -p %log% -s -m *.* /D -10 /C "cmd /c del @path"
  
