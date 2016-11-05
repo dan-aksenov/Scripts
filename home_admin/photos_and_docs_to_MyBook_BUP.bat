1330dac95987f3758eb118278715b60b819e0e3a
dan-aksenov <dan-aksenov@yandex.ru>
2016-11-06 01:36:46 +0300

    home backups
    backup to mybook fixes

--- home_admin/photos_and_docs_to_MyBook_BUP.bat
+++ home_admin/photos_and_docs_to_MyBook_BUP.bat
@@ -2,18 +2,25 @@ chcp 1251
 set log=d:\tmp
 IF NOT EXIST %log% MD %log%
 
+rem Exclude folders
 set excl=YandexDisk "VirtualBox VMs" torrent Desktop Downloads
 
+rem robocopy parameters for list and copy commands
+set list_prm=/l /mir /njh /ndl /nfl
+set copy_prm=/TEE /z /MIR /mt
+
+rem move backup to backups old
 set src=K:\HOME_BUP
 set dst=K:\HOME_BUP_OLD
-robocopy %src% %dst% *.* /xd %excl% /l /mir /njh /ndl /nfl
+robocopy %src% %dst% *.* /xd %excl% %list_prm%
 pause
-robocopy %src% %dst% *.* /unilog:"%log%\home_to_old.log" /xd %excl% /TEE /z /MIR /mt
+robocopy %src% %dst% *.* /unilog:"%log%\home_to_old.log" /xd %excl% %copy_prm%
 pause
 
+rem backups 
 set src="d:\Users\Данила"
-set dsc=K:\HOME_BUP
-robocopy %src% %dst% *.* /xd %excl% /l /mir /njh /ndl /nfl
+set dst=K:\HOME_BUP
+robocopy %src% %dst% *.* /xd %excl% %list_prm%
 pause
-robocopy %src% %dst% *.* /unilog:"%log%\home_to_mybook.log" %excl% /TEE /z /MIR /mt
+robocopy %src% %dst% *.* /unilog:"%log%\home_to_mybook.log" /xd %excl% %copy_prm%
 pause
