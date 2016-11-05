c5d29e176d10a667d9c13a1ba0be3aec7bca6c50
dan-aksenov <dan-aksenov@yandex.ru>
2016-11-06 00:29:28 +0300

    home backups
    minor fixes

--- home_admin/photos_and_docs_to_MyBook_BUP.bat
+++ home_admin/photos_and_docs_to_MyBook_BUP.bat
@@ -1,21 +1,19 @@
 chcp 1251
-
-rem todo: add options for incr and full
-
 set log=d:\tmp
-
 IF NOT EXIST %log% MD %log%
+
+set excl=YandexDisk "VirtualBox VMs" torrent Desktop Downloads
 
 set src=K:\HOME_BUP
 set dst=K:\HOME_BUP_OLD
-robocopy %src% %dst% *.* /xd YandexDisk "VirtualBox VMs" /l /mir /njh /ndl /nfl
+robocopy %src% %dst% *.* /xd %excl% /l /mir /njh /ndl /nfl
 pause
-robocopy %src% %dst% *.* /unilog:"%log%\home_to_old.log" /xd YandexDisk "VirtualBox VMs" /TEE /z /MIR /mt
+robocopy %src% %dst% *.* /unilog:"%log%\home_to_old.log" /xd %excl% /TEE /z /MIR /mt
+pause
 
 set src="d:\Users\Данила"
 set dsc=K:\HOME_BUP
-robocopy %src% %dst% *.* /xd YandexDisk "VirtualBox VMs" /l /mir /njh /ndl /nfl
+robocopy %src% %dst% *.* /xd %excl% /l /mir /njh /ndl /nfl
 pause
-robocopy %src% %dst% *.* /unilog:"%log%\home_to_mybook.log" /xd YandexDisk "VirtualBox VMs" /TEE /z /MIR /mt
-
+robocopy %src% %dst% *.* /unilog:"%log%\home_to_mybook.log" %excl% /TEE /z /MIR /mt
 pause
