d8d7640a6be4085e4b77d6981036164c4adbf15c
dan-aksenov <dan-aksenov@yandex.ru>
2016-11-05 16:20:23 +0300

    home backups
    minor fixes

--- home_admin/photos_and_docs_to_MyBook_BUP.bat
+++ home_admin/photos_and_docs_to_MyBook_BUP.bat
@@ -8,10 +8,10 @@ IF NOT EXIST %log% MD %log%
 
 set src=K:\HOME_BUP
 set dst=K:\HOME_BUP_OLD
-robocopy %src% %dst% *.* /e /unilog:"%log%\home_to_old.log" /xd YandexDisk /TEE /z /purge
+robocopy %src% %dst% *.* /e /unilog:"%log%\home_to_old.log" /xd YandexDisk "VirtualBox VMs"  /TEE /z /purge
 
 set src="d:\Users\Данила"
 set dsc=K:\HOME_BUP
-robocopy %src% %dst% *.* /e /unilog:"%log%\home_to_mybook.log" /xd YandexDisk /TEE /z /purge
+robocopy %src% %dst% *.* /e /unilog:"%log%\home_to_mybook.log" /xd YandexDisk "VirtualBox VMs" /TEE /z /purge
 
 pause
