5d79ea90b8fcd3723ec021aaa867a77b836831e4
dan-aksenov <dan-aksenov@yandex.ru>
2016-11-06 01:49:36 +0300

    home backups
    home to mybook

--- home_admin/photos_and_docs_to_MyBook_BUP.bat
+++ home_admin/photos_and_docs_to_MyBook_BUP.bat
@@ -9,18 +9,24 @@ rem robocopy parameters for list and copy commands
 set list_prm=/l /mir /njh /ndl /nfl
 set copy_prm=/TEE /z /MIR /mt
 
-rem move backup to backups old
+rem backup to backup old
 set src=K:\HOME_BUP
 set dst=K:\HOME_BUP_OLD
 robocopy %src% %dst% *.* /xd %excl% %list_prm%
-pause
-robocopy %src% %dst% *.* /unilog:"%log%\home_to_old.log" /xd %excl% %copy_prm%
+
 pause
 
-rem backups 
+robocopy %src% %dst% *.* /unilog:"%log%\home_to_old.log" /xd %excl% %copy_prm%
+
+pause
+
+rem backup itself
 set src="d:\Users\Данила"
 set dst=K:\HOME_BUP
 robocopy %src% %dst% *.* /xd %excl% %list_prm%
+
 pause
+
 robocopy %src% %dst% *.* /unilog:"%log%\home_to_mybook.log" /xd %excl% %copy_prm%
+
 pause
