8b869583d2530d1f0b25b92ee03daf0017b8ec66
dan-aksenov <dan-aksenov@yandex.ru>
2017-01-14 20:54:38 +0300

    (

--- home_admin/PHOTOS_to_CLOUDS_BUP.bat
+++ home_admin/PHOTOS_to_CLOUDS_BUP.bat
@@ -1,19 +1,15 @@
 chcp 1251
 
-rem todo: add options for incr and full
-
 set log_dir="d:\TMP"
-rem set src="d:\Users\Данила"
 
-
-IF NOT EXIST %log% MD %log%
+IF NOT EXIST %log_dir% MD %log_dir%
 
 rem set dst="d:\Users\Данила\YandexDisk\!BUPS"
 rem set log=%log_dir%\photo_to_Ya.log
 rem start call %cp_cmd%
 
 set src="d:\Users\Данила\Pictures\Мои фотографии"
-set dst="z:\!BUPS\fotos\Мои фотографии"
+set dst="\\Wdmycloud\danila\!BUPS\fotos\Мои фотографии"
 set log=%log_dir%\photo_to_mycloud.log
 robocopy %src% %dst% *.* %cp_prm% /unilog:%log% /tee /z /purge /s /mt /xd YandexDisk
 
