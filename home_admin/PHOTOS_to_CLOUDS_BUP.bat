09ac04219476c277637adb26e7a403e1b2dc56fe
dan-aksenov <dan-aksenov@yandex.ru>
2017-01-14 20:07:25 +0300

    fixes

--- home_admin/PHOTOS_to_CLOUDS_BUP.bat
+++ home_admin/PHOTOS_to_CLOUDS_BUP.bat
@@ -2,16 +2,19 @@ chcp 1251
 
 rem todo: add options for incr and full
 
-set log="d:\TMP"
+set log_dir="d:\TMP"
 set src="d:\Users\Данила"
+set cp_prm="/z /purge /mt /xd YandexDisk"
 
 IF NOT EXIST %log% MD %log%
 
 set dst="d:\Users\Данила\YandexDisk\!BUPS"
-start call robocopy %src% %dst% *.* /s /unilog:%log%\photo_to_Ya.log /z /purge /mt /xd YandexDisk
+set log=%log_dir%\photo_to_Ya.log
+start call robocopy %src% %dst% *.* /s /unilog:%log% %cp_prm%
 
 set dst="\\wdmycloud\danila\!BUPS"
-start call robocopy %src% %dst% *.* /s /unilog:%log%\photo_to_mycloud.log /z /purge /mt /xd YandexDisk
+set log=%log_dir%\photo_to_mycloud.log
+start call robocopy %src% %dst% *.* /s /unilog:%log% %cp_prm%
 
 rem forfiles -p %log% -s -m *.* /D -10 /C "cmd /c del @path"
  
