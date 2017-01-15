f1080f74601033c12ef9e8061d9e35edf8235d87
dan-aksenov <dan-aksenov@yandex.ru>
2017-01-15 15:31:03 +0300

    minor fixes

--- home_admin/PHOTOS_to_CLOUDS_BUP.bat
+++ home_admin/PHOTOS_to_CLOUDS_BUP.bat
@@ -1,17 +1,21 @@
 chcp 1251
 
-set log_dir="d:\TMP"
+set log_dir=d:\TMP
 
 IF NOT EXIST %log_dir% MD %log_dir%
 
-rem set dst="d:\Users\Данила\YandexDisk\!BUPS"
-rem set log=%log_dir%\photo_to_Ya.log
-rem start call %cp_cmd%
+set src="d:\Users\Данила\Pictures\Мои фотографии"
+set dst="d:\Users\Данила\YandexDisk\!BUPS\fotos\Мои фотографии"
+set log=/unilog:"%log_dir%\photo_to_Ya.log"
+
+robocopy %src% %dst% *.* %log% /tee /z /mir /mt
+
+pause
 
 set src="d:\Users\Данила\Pictures\Мои фотографии"
 set dst="\\Wdmycloud\danila\!BUPS\fotos\Мои фотографии"
-set log=%log_dir%\photo_to_mycloud.log
-robocopy %src% %dst% *.* %cp_prm% /unilog:%log% /tee /z /purge /s /mt /xd YandexDisk
+set log=/unilog:"%log_dir%\photo_to_mycloud.log"
+robocopy %src% %dst% *.* %log% /tee /z /mir /mt
 
 rem forfiles -p %log% -s -m *.* /D -10 /C "cmd /c del @path"
  
