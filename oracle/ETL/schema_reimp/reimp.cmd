3dbf2c715d677674e69b6a8abc4654b3a45a58dd
dan-aksenov <dan-aksenov@yandex.ru>
2017-02-20 15:52:57 +0300

    minor fixes

--- oracle/ETL/schema_reimp/reimp.cmd
+++ oracle/ETL/schema_reimp/reimp.cmd
@@ -1,3 +1,6 @@
-rem			dest schema src
-sqlplus user/pass@%1 @drop %2
-impdp user/pass@%1 schemas=%2 network_link=%3 logfile=%2 parallel=20
+rem	1 - dest 2 - schema 3 - src
+
+set conn_string=user/pass@%1
+
+sqlplus %conn_string% @drop %2
+impdp %conn_string% schemas=%2 network_link=%3 logfile=%2 parallel=20
