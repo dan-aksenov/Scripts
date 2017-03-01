89f6f99a463bea23136a0fcfc068065239e5593f
dan-aksenov <dan-aksenov@yandex.ru>
2017-03-01 18:43:19 +0300

    initial commit

---
+++ linux/snort_install.sh
@@ -0,0 +1,12 @@
+yum -y install https://www.snort.org/downloads/snort/daq-2.0.6-1.centos7.x86_64.rpm
+                      
+yum -y install https://www.snort.org/downloads/snort/snort-2.9.9.0-1.centos7.x86_64.rpm
+
+# download rules
+
+# setup snort.conf
+
+# barnyard2 - compile from souce
+# need to download and compile
+# libdnet-1.11.tar.gz
+# libpcap-1.8.1.tar.gz
