2f4c39903a10a410f6ca70714fbed7c1a770b4e3
dan-aksenov <dan-aksenov@yandex.ru>
2017-03-03 11:47:04 +0300

    release version
    mostly

--- linux/snort_install.sh
+++ linux/snort_install.sh
@@ -4,8 +4,11 @@ yum -y install https://www.snort.org/downloads/snort/snort-2.9.9.0-1.centos7.x86
 
 # download rules. need to get correct link for snapshot-2990
 cd ~/Downloads
+# daq & snort 
 wget https://www.snort.org/downloads/registered/snortrules-snapshot-2990.tar.gz
 wget https://www.snort.org/downloads/community/community-rules.tar.gz
+# base
+wget http://sourceforge.net/projects/secureideas/files/BASE/base-1.4.5/base-1.4.5.tar.gz
 cd
 
 # setup snort.conf
@@ -55,4 +58,32 @@ sudo -u postgres psql snort -f /tmp/create_postgresql
 
 sudo mkir /var/log/barnyard2
 
+cat > /lib/systemd/system/barnyard2.service << EOF
+[Unit]
+Description=Barnyard2 Daemon
+After=syslog.target network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/barnyard2 -c /etc/barnyard2.conf -d /var/log/snort -f snort.u2 -w /var/log/snort/barnyard2.waldo -g snort -u snort
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl start snort
+sudo systemctl enable snort
+
+# Installing base
 yum install httpd yum-utils php-pear php-pgsql php-mbstring php php-adodb
+
+sudo mkdir /var/www/base/
+sudo mv base-1.4.5/* /var/www/base/
+chown -R apache.apache /var/www/base
+
+cat > /etc/httpd/conf.d/base.conf << EOF
+Alias /base/ /var/www/base/
+<Directory "/var/www/base/">
+  AllowOverride All
+</Directory>
+EOF
