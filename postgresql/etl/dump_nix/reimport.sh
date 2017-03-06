a1075265e30b923b3fc84217023f866c0ae77d04
dan-aksenov <dan-aksenov@yandex.ru>
2017-03-06 18:13:34 +0300

    reimport scripts merged

--- postgresql/etl/dump_nix/reimport.sh
+++ postgresql/etl/dump_nix/reimport.sh
@@ -1,33 +1,35 @@
-# postgresql reimport srcipt
 display_usage() {
         echo "Correct usage:"
-        echo "reimp.sh source_host source_db destination_db_owner"
+        echo "1 - dest db(to be droped), 2 - owner, 3 - source db, 4 -source host(defaults to localhost) "
         }
 
+dst_db=$1
+owner=$2
+src_db=$3
+scr_host=$4
+
 # if less than 3 variables supplied
-if [  $# -ne 3 ]
+if [  $# -lt 3 ]
 then
         display_usage
         exit 1
 fi
 
-src_host=$1
-src_db=$2
-db_owner=$3
+if [ !$scr_host ]
+then
+        scr_host=localhost
+fi
 
-# source
-ssh $src_host pg_dump --format custom --blobs --verbose --file /tmp/$src_db.dmp $src_db
-scp $src_host:/tmp/$src_db.dmp /tmp/$src_db.dmp
+# Drop dest database, with session disconnect and empty database creation.
+psql <<EOF
+select pg_terminate_backend(pid) from pg_stat_activity where datname = '$dst_db';
+drop database $dst_db;
+create database $dst_db with owner $owner;
+EOF
 
-# target
-# recreate db
-psql -c "select pg_terminate_backend(pid) from pg_stat_activity where datname = 'src_db'"
-psql -c "drop database $src_db"
-psql -c "create database $src_db WITH OWNER = $db_owner"
-
-# restore
-pg_restore --dbname $src_db --verbose /tmp/$src_db.dmp
+# Reimport database from source
+pg_dump -h "$src_host" --blobs $src_db | psql $dst_db &>/tmp/restore.log
 
 # postsrcipt
-psql -c "UPDATE pg_language set lanpltrusted = true where lanname='pltclu'" $src_db
-psql -c "UPDATE pg_language set lanpltrusted = true where lanname='plpythonu'" $src_db
+psql -c "UPDATE pg_language set lanpltrusted = true where lanname='pltclu'" $dst_db
+psql -c "UPDATE pg_language set lanpltrusted = true where lanname='plpythonu'" $dst_db
