1558254b9dfa5f1ebf9ab40363af4f48e8e55cb3
dan-aksenov <dan-aksenov@yandex.ru>
2017-03-22 11:22:56 +0300

    comments added

--- postgresql/backup_and_recovery/setup_for_barman.sh
+++ postgresql/backup_and_recovery/setup_for_barman.sh
@@ -1,6 +1,11 @@
+# This script should be runned from backup server.
+# pg_host - is the PostgreiSQL server being added to barman's infrastructure.
 pg_host=$1
 
+# Barman should be able to connect to database server via ssh
 ssh-copy-id postgres@$pg_host
+
+# Creating database server configuration file.
 cat > /tmp/$pg_host.conf << EOF
 [$pg_host]
 description =  "$pg_host PostgreSQL server"
@@ -12,10 +17,11 @@ slot_name = barman
 EOF
 sudo cp /tmp/$pg_host.conf /etc/barman.d/
 
+# Creating barman users in target database.
 createuser -h $pg_host -s -W -P -U postgres barman 
 createuser -h $pg_host -s -W -P -U postgres --replication streaming_barman 
 
-# add passw to .pgpass on barman server
+# Add passw to .pgpass on barman server
 cat >>.pgpass << EOF
 $pg_host:5432:postgres:barman:barman
 $pg_host:5432:postgres:streaming_barman:barman
@@ -23,6 +29,7 @@ EOF
 
 # to do: add pg_hba on target!
 
+# Check if barman can actually connect to target database.
 psql -c 'SELECT version()' -U barman -h $pg_host postgres
 psql -U streaming_barman -h $pg_host -c "IDENTIFY_SYSTEM" replication=1
 
@@ -31,8 +38,10 @@ ssh postgres@$pg_host sed -i -e "s/#wal_level = 'minimal'/wal_level = 'hot_stand
 ssh postgres@$pg_host sed -i -e "s/#max_wal_senders = 0/max_wal_senders = 2/g" /var/lib/pgsql/*/data/postgresql.conf
 ssh postgres@$pg_host sed -i -e "s/#max_replication_slots = 0/max_replication_slots = 2/g" /var/lib/pgsql/*/data/postgresql.conf
 
+# Create replication slot on barman server
 barman receive-wal --create-slot $pg_host
 
+# Barman commands cheatsheet
 barman cron
 barman switch-xlog --force $pg_host
 barman check $pg_host
