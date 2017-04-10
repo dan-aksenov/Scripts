fc1733aebe0e44f8b93462526978f21644909568
dan-aksenov <dan-aksenov@yandex.ru>
2017-04-10 15:28:05 +0300

    redone with datapump

--- oracle/ETL/dump_exp.cmd
+++ oracle/ETL/dump_exp.cmd
@@ -1,10 +1,20 @@
-rem very simple export script for logical backup on windows
+rem name for archive file
 set arch=%date:~-10,2%%date:~-7,2%%date:~-4,4%
 
-exp userid= '"sys/passwd as sysdba'"  full=y log=log.txt direct=y
+rem daily or weekly backup
+tag=backup_week
 
-rar a -parchpasswd %arch% -m5 expdat.dmp log.txt
-move %arch%.rar dumps\
+rem directories
+set backup_dir=d:\%tag%\dumps
+set remote_dir=\\192.168.1.250\db_backups\%tag%
 
-rem delete older dumps
-forfiles -p ".\dumps" -s -m *.* /D -30 /C "cmd /c del @path"
+rem expdp and archive
+rem ORACLE directory with %tag% -name presumed existing.
+expdp '"sys/passwd as sysdba'"  full=y directory=%tag%
+rar a -pfilatoff %backup_dir%\%arch% -m5 -df %backup_dir%\expdat.dmp %backup_dir%\export.log
+
+rem delete old dumps
+forfiles -p "%backup_dir% -s -m *.* /D -40 /C "cmd /c del @path"
+
+rem copy to remote storage
+robocopy %backup_dir% %remote_dir% *.rar /z /s /MIR /mt /log:.\backup_to_lkk.log
