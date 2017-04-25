4ce7d24c30399555187890e36dbc3acc3b959781
dan-aksenov <dan-aksenov@yandex.ru>
2017-04-25 22:40:00 +0300

    docs backup script redone in ps
    documents folder now lodated automatically

---
+++ home_admin/docs_to_mycloud.ps1
@@ -0,0 +1,14 @@
+#logging directory
+$log_dir="E:\TMP"
+#if not exist $log_dir% md $log_dir
+
+#set source
+$src=[environment]::getfolderpath("mydocuments")
+
+#set Wdmycloud destination
+$dst="\\WDMYCLOUD\danila\!BUPS\Documents"
+
+$log="/unilog:$log_dir\docs_to_mycloud.log"
+
+robocopy $src $dst *.* $log /tee /z /mir /mt
+
