3e9a8b255c3935482eac75da53df1f65e558affe
dan-aksenov <dan-aksenov@yandex.ru>
2017-04-28 11:36:33 +0300

    work in progress. still need to apply archivelog retention...

--- postgresql/backup_and_recovery/basebackup.py
+++ postgresql/backup_and_recovery/basebackup.py
@@ -1,5 +1,5 @@
 #!/bin/python
-import sys, getopt, psycopg2, os, datetime
+import sys, getopt, psycopg2, os, datetime, time
 from subprocess import call
 
 # variables to be redone for python
@@ -13,14 +13,15 @@ from subprocess import call
 backup_label = str(datetime.date.today())
 pid = str(os.getpid())
 pidfile = "/tmp/basebackup.pid"
-age=3
-# /variables
+age=1
 
+# Function to display command usage.
 def usage(): 
     print "basebackup.py usage: "
     print "    required arument: -b /dir or --basebackup=/dir backup destination"
     sys.exit(2)
 
+# Check for arguments.
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'b:', ['backup_directory='])
 except getopt.GetoptError as err:
@@ -32,26 +33,35 @@ if len(sys.argv) <= 1:
 
 for opt, arg in opts:
     if opt in ('-b', '--backup_directory'):
-        backupdir = arg
+        backup_dir = arg
     else:
         usage()
 
-if not os.path.isdir(backupdir):
+# Check if supplied backup directory exists.
+if not os.path.isdir(backup_dir):
     print "No valid directory supplied :("
     usage()
 
+# Check if another backpu is already running.
 if os.path.isfile(pidfile):
     print "Another backup (pid %s) already running, exiting" % pid
     sys.exit()
 file(pidfile, 'w').write(pid)
 
-# todl: rewrite this with call('ls -l', shell=True)
-call([ "pg_basebackup", "-l", backup_label, "-U", "postgres", "-D", backupdir, "-F", "t", "-P", "-v", "-x", "-z"] )
+# Create labeled directory to hold timestamped backup
+final_dir = backup_dir + '/' + backup_label
+if not os.path.exists(final_dir):
+    os.makedirs(final_dir)
+
+# Create backup command string using label, directory and required parameters and run it.
+backup_command = "pg_basebackup -l" + backup_label + " -U postgres -D" + final_dir + " -F t -P -v -x -z"
+call(backup_command, shell=True)
 
 # todo: remove old backups and archivelogs.
 # not working as expected. revision needed
-for f in os.listdir(backupdir):
-    file = os.path.join(backupdir, f)
+current_time = time.time()
+for f in os.listdir(backup_dir):
+    file = os.path.join(backup_dir, f)
     creation_time = os.path.getctime(file)
     if (current_time - creation_time) // (24 * 3600) >= age:
         os.unlink(file)
