6d918a495d9704691eebe7eecc3b945a0fe5ceab
dan-aksenov <dan-aksenov@yandex.ru>
2017-04-28 15:45:53 +0300

    some work invested

--- postgresql/backup_and_recovery/basebackup.py
+++ postgresql/backup_and_recovery/basebackup.py
@@ -7,12 +7,23 @@ from glob import glob
 # variables to be redone for python
 #PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/pgsql-9.4/bin"
 #PGHOME=/var/lib/pgsql/9.4
-#PGDATA=$HOME/data # Not used for now //dbax
-#PGARCH=$PGHOME/backup/arch
 #PG_BASEBACKUP=$(which pg_basebackup)
 #PG_ARCHIVECLEANUP=$(which pg_archivecleanup)
 
+# make arch dir dynamic
 arch_dir = "/var/lib/pgsql/9.4/data/arch/"
+
+# Get archivelog dir not working yet
+import psycopg2
+
+try:
+    conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost'")
+except:
+    print "ERROR: unable to connect to the database!"
+cur = conn.cursor()
+cur.execute("show archive_command")
+#arch_dir = cur.fetchall()
+
 backup_label = str(datetime.date.today())
 pid = str(os.getpid())
 pidfile = "/tmp/basebackup.pid"
@@ -52,15 +63,16 @@ if os.path.isfile(pidfile):
 file(pidfile, 'w').write(pid)
 
 # Create labeled directory to hold timestamped backup.
-final_dir = backup_dir + '/' + backup_label
+final_dir = backup_dir + '/db_' + backup_label
 if not os.path.exists(final_dir):
     os.makedirs(final_dir)
 
+print "Creating postgresql cluster backup:"
 # Create backup command string using label, directory and required parameters and run it.
 backup_command = "pg_basebackup -l" + backup_label + " -U postgres -D" + final_dir + " -F t -P -v -x -z"
 call(backup_command, shell=True)
 
-# todo: remove old backups and archivelogs.
+print "Deleting old backups according to retention policy:"
 current_time = time.time()
 for f in os.listdir(backup_dir):
     file = os.path.join(backup_dir, f)
@@ -69,7 +81,7 @@ for f in os.listdir(backup_dir):
         rmtree(file)
         print('{} removed'.format(file))
 
-# Delete old archivelogs
+print "Deleteing old archivelogs:"
 for file in os.listdir(arch_dir):
     if file.endswith(".backup"):    
         arch_file = os.path.join(arch_dir, file)
