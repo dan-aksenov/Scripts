48f5cd88428ca9ae18c5a51bede59b344cdf87b0
dan-aksenov <dan-aksenov@yandex.ru>
2017-05-31 15:08:35 +0300

    params check added

--- linux/openvpn-client-conf.sh
+++ linux/openvpn-client-conf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # 1 - username, 2 - mailto
+if [ $# -lt 2 ]; then
+   echo Usage: 1 - vpn username, 2 - address to send config file
 username=$1
 mailto=$2
 easyrsa_location=/opt/easy-rsa-master/easyrsa3
