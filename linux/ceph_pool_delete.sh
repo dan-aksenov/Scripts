ef2bc8378161ce909ffc6aaefb62fcddee73907e
dan-aksenov <dan-aksenov@yandex.ru>
2017-09-19 15:57:29 +0300

    ceph pool delete added

---
+++ linux/ceph_pool_delete.sh
@@ -0,0 +1,16 @@
+old_pool=$1
+new_pool=new.$old_pool
+
+# Create a new pool, with the correct pg count:
+ceph osd pool create $new_pool 8
+
+# Copy the contents of the old pool the new pool:
+rados cppool $old_pool $new_pool
+
+# Remove the old pool:
+# Currently not remove, but rename.
+#ceph osd pool delete $old_pool $old_pool --yes-i-really-really-mean-it
+ceph osd pool rename $old_pool old.$old_pool
+
+# Rename the new pool to original name.
+ceph osd pool rename $new_pool $old_pool
