7b4e738002d8e7edb5be6efb828d2e513d5cee62
dan-aksenov <dan-aksenov@yandex.ru>
2017-09-28 13:52:26 +0300

    simple multiple host shell runner.

---
+++ linux/diy_cluster_shell.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cmd=$1
+if [ -z ${hosts+x} ]
+then
+   echo "No hosts selected. Declare hosts variable."
+   exit 1
+fi
+   if [ -z ${1+x} ]; then echo "No remote command set. Set it as first positional parameter."
+   exit 1
+fi
+
+for host in $hosts
+do
+   ssh $host $cmd
+done
