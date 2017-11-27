6252276a10537416f291628f47f4f147da90747c
dan-aksenov <dan-aksenov@yandex.ru>
2017-11-27 14:18:10 +0300

    fixes

--- linux/my_sql_dump.sh
+++ linux/my_sql_dump.sh
@@ -2,3 +2,4 @@
 now=$(date +"%m%d%Y_%H%M%S")
 dump_dir=$1
 mysqldump -u zabbix -pzabbix --all-databases 2>$dump_dir/mysql_dump_$now.log > $dump_dir/mysql_dump_$now.sql
+find $dump_dir -type f \( -name "*.sql" -o -name "*.log" \) -mtime +30 -delete
