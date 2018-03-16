844f70956855db5cbaac3e402f860373a8510919
dan-aksenov <dan-aksenov@yandex.ru>
2018-03-16 23:40:12 +0300

    path changed

--- home_admin/YA_DISK_BUP.bat
+++ home_admin/YA_DISK_BUP.bat
@@ -1,3 +1,3 @@
 chcp 1251
-robocopy d:\Users\Данила\YandexDisk\ \\Wdmycloud\danila\!BUPS\ya *.* /s /MIR /PURGE /xd !BUPS .sync
+robocopy E:\Users\Danila\YandexDisk\ \\Wdmycloud\danila\!BUPS\ya *.* /s /MIR /PURGE /xd !BUPS .sync
 pause
