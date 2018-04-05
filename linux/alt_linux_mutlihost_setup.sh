51698b484f40f963a66fd73516c3ca0fa5bb91ca
dan-aksenov <dan-aksenov@yandex.ru>
2018-04-05 17:02:17 +0300

    tomcat added

--- linux/alt_linux_mutlihost_setup.sh
+++ linux/alt_linux_mutlihost_setup.sh
@@ -25,6 +25,7 @@ for i in $hosts; do ssh $i chkconfig zabbix_agentd on; done
 for i in $hosts; do ssh $i sed -i 's/^Server=127.0.0.1/Server=pts-test-zab1c/g' /etc/zabbix/zabbix_agentd.conf; done
 for i in $hosts; do ssh $i sed -i 's/^ServerActive=127.0.0.1/ServerActive=pts-test-zab1c/g' /etc/zabbix/zabbix_agentd.conf; done
 for i in $hosts; do ssh $i sed -i 's/.localdomain//g' /etc/zabbix/zabbix_agentd.conf; done
+for i in $hosts; do ssh $i service zabbix_agentd start; done
 
 #postgres
 #for i in $dbs; do scp /etc/apt/sources.list.d/pgpro.list $i:/etc/apt/sources.list.d/pgpro.list; done
@@ -35,3 +36,18 @@ for i in $dbs; do ssh $i apt-get update; done
 for i in $dbs; do ssh mkdir /var/lib/pgsql/9.6/pts/{fdc_pts_big_ind,fdc_pts_big_tab,fdc_pts_ind,fdc_pts_tab} -p; done
 for i in $dbs; do ssh $i mkdir /var/lib/pgsql/9.6/pts/{fdc_trans_ind,fdc_trans_tab} -p; done
 for i in $dbs; do ssh $i chown postgres.postgres /var/lib/pgsql/9.6/pts -R; done
+
+#tomcat
+for i in $apps; do ssh $i useradd tomcat; done
+for i in $apps; do ssh $i mkdir /root/distr /u01; done
+for i in $apps; do scp /root/distr/jdk-8u161-linux-x64.tar.gz  $i:/root/distr/jdk-8u161-linux-x64.tar.gz; done
+for i in $apps; do scp /root/distr/apache-tomcat-8.5.29.tar.gz  $i:/root/distr/apache-tomcat-8.5.29.tar.gz; done
+for i in $apps; do ssh $i tar -xf /root/distr/jdk-8u161-linux-x64.tar.gz -C /u01; done
+for i in $apps; do ssh $i tar -xf /root/distr/apache-tomcat-8.5.29.tar.gz -C /u01; done
+for i in $apps; do ssh $i chown tomcat.tomcat /u01/jdk1.8.0_161 -R; done
+for i in $apps; do ssh $i chown tomcat.tomcat /u01/apache-tomcat-8.5.29 -R; done
+for i in $apps; do ssh $i mkdir /u01/apache-tomcat-8.5.29/logs -p; done
+for i in $apps; do scp /root/distr/tomcat_users  $i:/u01/apache-tomcat-8.5.29/conf/tomcat-users.xml; done
+for i in $apps; do scp /root/distr/tomcat.service  $i:/etc/systemd/system/tomcat.service; done
+for i in $apps; do scp /root/distr/manager_context  $i:/u01/apache-tomcat-8.5.29/webapps/manager/META-INF/context.xml; done
+for i in $apps; do ssh $i chown tomcat.tomcat /u01/apache-tomcat-8.5.29/webapps/manager/META-INF/context.xml; done
