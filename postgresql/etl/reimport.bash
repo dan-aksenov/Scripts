306be8c62a948cbbd7952f63ead2d7632b5d7f44
dan-aksenov <dan-aksenov@yandex.ru>
2018-04-19 15:12:34 +0300

    comments added

--- postgresql/etl/reimport.bash
+++ postgresql/etl/reimport.bash
@@ -7,4 +7,10 @@ function reimp {
     psql -p 5433 -c "create database $1"
     pg_dump -Fp -p 5432 $1 | psql -p 5433 $1 &>/tmp/$1.log }
 export -f reimp
-time parallel -j 4 --load 50% reimp '{}' ::: $dbs
+
+#--eta: Shows the estimated time remaining to run all jobs.
+#--jobs 2: The number of commands to run at the same time, which in this case was set to 2.
+#--load 80%": The maximum CPU load at which new jobs will not be started.
+#--noswap": New jobs won't be started if there is both swap-in and swap-out activity.
+
+time parallel -j 4 --load 50% --noswap reimp '{}' ::: $dbs
