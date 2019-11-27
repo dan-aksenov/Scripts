53069545c2a954f28957d29802a5023a23372883
dbax <dan-aksenov@yandex.ru>
2019-11-27 14:46:49 +0300

    pg_ctl nowait added

--- postgresql/setup/simple_major_upgrade.sh
+++ postgresql/setup/simple_major_upgrade.sh
@@ -7,7 +7,7 @@ master=cos7-sb1
 
 ansible-playbook -i ../ansible-hosts/test -l $master postgres_main.yml --tags 1_repository,2_install,3_init -e "postgresql_version=$pg_ver_new"
 
-ssh ansible@$master sudo -u postgres /usr/pgsql-$pg_ver_old/bin/pg_ctl stop -D /var/lib/pgsql/$pg_ver_old/data
+ssh ansible@$master sudo -u postgres /usr/pgsql-$pg_ver_old/bin/pg_ctl stop -W -D /var/lib/pgsql/$pg_ver_old/data
 
 ssh ansible@$master sudo -u postgres cp /var/lib/pgsql/$pg_ver_old/data/conf.d/  /var/lib/pgsql/$pg_ver_new/data/ -R
 ssh ansible@$master sudo -u postgres cp /var/lib/pgsql/$pg_ver_old/data/postgresql.conf /var/lib/pgsql/$pg_ver_new/data/postgresql.conf
@@ -17,4 +17,4 @@ cmd="/usr/pgsql-$pg_ver_new/bin/pg_upgrade -d /var/lib/pgsql/$pg_ver_old/data -D
 
 ansible -i ../ansible-hosts/test $master -a "$cmd" --become --become-user=postgres -u ansible 
 
-ssh ansible@$master sudo -u postgres /usr/pgsql-$pg_ver_new/bin/pg_ctl start -D /var/lib/pgsql/$pg_ver_new/data
+ssh ansible@$master sudo -u postgres /usr/pgsql-$pg_ver_new/bin/pg_ctl start -W -D /var/lib/pgsql/$pg_ver_new/data
