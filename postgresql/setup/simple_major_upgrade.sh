9ec7b1701e331cfaabbad7f42c2c8c9605354c54
dbax <dan-aksenov@yandex.ru>
2019-11-27 17:18:30 +0300

    slave upgrade added

--- postgresql/setup/simple_major_upgrade.sh
+++ postgresql/setup/simple_major_upgrade.sh
@@ -2,8 +2,9 @@
 pg_ver_old=$1
 pg_ver_new=$2
 master=cos7-sb1
+slave=cos7-sb2
 
-ansible-playbook -i ../ansible-hosts/test -l $master postgres_main.yml --tags 1_repository,2_install,3_init -e "postgresql_version=$pg_ver_new"
+ansible-playbook -i ../ansible-hosts/test -l $master,$slave postgres_main.yml --tags 1_repository,2_install,3_init -e "postgresql_version=$pg_ver_new"
 
 ansible -i ../ansible-hosts/test $master -a "/usr/pgsql-$pg_ver_old/bin/pg_ctl stop -D /var/lib/pgsql/$pg_ver_old/data" --become --become-user=postgres -u ansible 
 
@@ -13,6 +14,13 @@ ssh ansible@$master sudo -u postgres cp /var/lib/pgsql/$pg_ver_old/data/pg_hba.c
 
 cmd="/usr/pgsql-$pg_ver_new/bin/pg_upgrade -d /var/lib/pgsql/$pg_ver_old/data -D /var/lib/pgsql/$pg_ver_new/data -b /usr/pgsql-$pg_ver_old/bin/ -B /usr/pgsql-$pg_ver_new/bin/ -k"
 ansible -i ../ansible-hosts/test $master -a "$cmd" --become --become-user=postgres -u ansible 
+
+ansible -i ../ansible-hosts/test $slave -a "/usr/pgsql-$pg_ver_old/bin/pg_ctl stop -D /var/lib/pgsql/$pg_ver_old/data" --become --become-user=postgres -u ansible 
+
+cmd="cd /var/lib/pgsql && rsync --relative --archive --hard-links --size-only $pg_ver_old/data $pg_ver_new/data root@$slave:/var/lib/pgsql/"
+ssh ansible@$master $cmd
+
+ansible-playbook -i ../ansible-hosts/test -l $slave postgres_main.yml --tags 6_startdb -e "postgresql_version=$pg_ver_new"
 
 ansible-playbook -i ../ansible-hosts/test -l $master postgres_main.yml --tags 6_startdb -e "postgresql_version=$pg_ver_new"
 #ansible -i ../ansible-hosts/test $master -a "/usr/pgsql-$pg_ver_new/bin/pg_ctl start -D /var/lib/pgsql/$pg_ver_new/data" --become --become-user=postgres -u ansible 
