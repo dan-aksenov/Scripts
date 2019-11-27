411d94cdc616834f364b1511e958195e365709dc
dbax <dan-aksenov@yandex.ru>
2019-11-27 14:13:38 +0300

    versions parametrized

--- postgresql/setup/simple_major_upgrade.sh
+++ postgresql/setup/simple_major_upgrade.sh
@@ -1,10 +1,9 @@
 # Postgresql version
-pg_ver_old=11
-pg_ver_new=12
-# Streaming master
+set -e
+
+pg_ver_old=$1
+pg_ver_new=$2
 master=cos7-sb1
-# Streaming slaves.
-#slave=cos7-sb2
 
 ansible-playbook -i ../ansible-hosts/test -l $master postgres_main.yml --tags 1_repository,2_install,3_init -e "postgresql_version=$pg_ver_new"
 
@@ -16,7 +15,6 @@ ssh ansible@$master sudo -u postgres cp /var/lib/pgsql/$pg_ver_old/data/pg_hba.c
 
 cmd="/usr/pgsql-$pg_ver_new/bin/pg_upgrade -d /var/lib/pgsql/$pg_ver_old/data -D /var/lib/pgsql/$pg_ver_new/data -b /usr/pgsql-$pg_ver_old/bin/ -B /usr/pgsql-$pg_ver_new/bin/ -k"
 
-ansible -i ../ansible-hosts/test $master -a "$cmd" --become --become-user=postgres -u ansible || exit $?
-ansible -i ../ansible-hosts/test $master -a "$cmd" --become --become-user=postgres -u ansible || exit $?
+ansible -i ../ansible-hosts/test $master -a "$cmd" --become --become-user=postgres -u ansible 
 
 ssh ansible@$master sudo -u postgres /usr/pgsql-$pg_ver_new/bin/pg_ctl start -D /var/lib/pgsql/$pg_ver_new/data
