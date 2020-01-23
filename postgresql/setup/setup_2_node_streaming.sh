4e939684e025fb2ae1a08a8061a7223c1a166df6
dbax <dan-aksenov@yandex.ru>
2020-01-23 12:20:25 +0300

    reverted to all ansible

--- postgresql/setup/setup_2_node_streaming.sh
+++ postgresql/setup/setup_2_node_streaming.sh
@@ -1,90 +1,36 @@
-# Postgresql installation on Centos7
-# Variables section
-PGVER=$1
-# User default postgres version if not set
-if [ -z "$PGVER" ]; then PGVER=9.5; fi
-# Postgres version without dots.
-PGVER2=$(echo $PGVER | sed -e "s/\.//g")
-# Determine Distribution name
-DIST=$(cat /etc/*-release | grep ^NAME)
-if [[ $DIST == *"CentOS"* ]]; then DIST=centos;
-	elif [[ $DIST == *"Oracle"* ]]; then DIST=oraclelinux;
-	else echo echo "Could not determine distribution name. Setup aborted." && exit 1;
-fi
-# End of variables section.
+# Should use My ansible roles to install postgreses.
+# User "ansible" with passwordless ssh to targeted hosts is required
 
-# Check connection
-wget --spider https://download.postgresql.org/pub/repos/yum/$PGVER/redhat/rhel-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-if [ "$?" != 0 ]; then
-  echo "Unable to connect to yum.postgresql.org. Check network of proxy settints."
+if [ "$#" -ne 4 ]; then
+    echo "ERROR! Need Parameters: 1 - Postgres version, 2 - Master's name, 3 - Slave's name, 4 - Ansible inventory file" 
+    exit 1
 fi
 
-# Add Posgresql repo
-yum -y install https://download.postgresql.org/pub/repos/yum/$PGVER/redhat/rhel-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+# Postgresql version
+pg_ver=$1
+# Streaming master
+master=$2
+# Streaming slaves.
+slave=$3
+# Ansible Inventory file
+inventory=$4
 
-# Install postgres main stuff
-yum -y install postgresql$PGVER2 postgresql$PGVER2-server postgresql$PGVER2-contrib postgresql$PGVER2-libs 
+echo Install Master
+ansible-playbook -i $inventory -l $master postgres_main.yml -e "postgresql_version=$pg_ver"
+echo Install Slave
+ansible-playbook -i $inventory -l $slave postgres_main.yml --tags slave -e "postgresql_version=$pg_ver"
 
-# Install some languages if needed
-#yum -y install  postgresql$PGVER2-plpython postgresql$PGVER2-pltcl postgresql$PGVER2-python
+echo Edit repmgr.conf
+ssh ansible@$master sudo sed -i 's/^#node_id=1/node_id=1/g' /etc/repmgr/$pg_ver/repmgr.conf
+ssh ansible@$slave sudo sed -i 's/^#node_id=1/node_id=2/g' /etc/repmgr/$pg_ver/repmgr.conf
 
-# is devel really needed?
-#- python-psycopg2
-#- pg_stat_kcache{{ postgresql_version | replace('.', '') }}
-#- powa_{{ postgresql_version | replace('.', '') }}
-#- pg_qualstats{{ postgresql_version | replace('.', '') }}
-#- hypopg_{{ postgresql_version | replace('.', '') }}
-#- powa_{{ postgresql_version | replace('.', '') }}-web
-#- repmgr{{ postgresql_version | replace('.', '') }}
+echo Register Master
+ssh ansible@$master sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr primary register
+echo Clone Slave
+ssh ansible@$slave sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr -h $master -U repmgr -d repmgr standby clone
+echo Start Slave
+ansible -i $inventory $slave -a "/usr/pgsql-$pg_ver/bin/pg_ctl start -D /var/lib/pgsql/$pg_ver/data" --become --become-user=postgres -u ansible 
+echo Register Slave
+ssh ansible@$slave sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr standby register
 
-
-# Install devel if needed (might be usefull sometimes)
-# is devel really needed?
-#yum -y install postgresql$PGVER2-devel 
-
-# Update-alternatives. do we need this?
-#a=0
-#ls /usr/pgsql-$PGVER/bin/ | grep -v postmaster | xargs -I{} sudo update-alternatives --install /usr/bin/{} pgsql-{}  /usr/pgsql-$PGVER/bin/{} $PGVER2$a
-
-# Add firewall exception for postgres. 
-firewall-cmd --permanent --zone=public --add-service=postgresql
-firewall-cmd --reload
-
-read -p "Initialize new database cluster (y/n):" INIT_FLAG
-if [ $INIT_FLAG = "y" ];
-	then
-	# Initialize DB (defauld directory)
-	/usr/pgsql-$PGVER/bin/postgresql$PGVER2-setup initdb
-	# Edit postgresql.conf: change listen address (might need to add more)
-	sed -i -e "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/pgsql/$PGVER/data/postgresql.conf
-	# Add entries to pg_hba.conf
-	echo 'host all all 127.0.0.1/32 md5' | sudo tee --append /var/lib/pgsql/$PGVER/data/pg_hba.conf
-	echo 'host all all 192.168.0.1/16 md5' | sudo tee --append /var/lib/pgsql/$PGVER/data/pg_hba.conf
-	# Enable and start daemon
-	systemctl enable postgresql-$PGVER
-	systemctl start postgresql-$PGVER
-	# Change superuser password
-	sudo -u postgres psql -c "alter user postgres password 'postgres'";
-	
-	# Install and run pgtune on database cluster
-	#PGCONF=/var/lib/pgsql/$PGVER/data/postgresql.conf # get it dynamical somehow
-	#yum install epel-release -y
-	#yum install pgtune -y
-	#pgtune -i $PGCONF -o $PGCONF.tuned -T Mixed -c 100
-	#mv $PGCONF $PGCONF.orig
-	#mv $PGCONF.tuned $PGCONF
-
-	cat >> $PGCONF << EOF
-#add stat_statements to libs
-shared_preload_libraries = 'pg_stat_statements'
-#add activities tracking
-track_activities = on
-track_counts = on
-track_io_timing = on
-track_functions = all
-log_autovacuum_min_duration = 0		
-#custom setting for stat_statements
-pg_stat_statements.track = all
-EOF
-chown postgres.postgres $PG_CONF
-fi
+ssh ansible@$slave sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr cluster show
