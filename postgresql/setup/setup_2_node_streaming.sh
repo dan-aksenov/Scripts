54f9f7c461291493f585ef88576682a3aedcc711
dbax <dan-aksenov@yandex.ru>
2020-06-25 16:46:37 +0300

    ansible user variablized

--- postgresql/setup/setup_2_node_streaming.sh
+++ postgresql/setup/setup_2_node_streaming.sh
@@ -1,8 +1,8 @@
-# Should use My ansible roles to install postgreses.
-# User "ansible" with passwordless ssh to targeted hosts is required
+# Using My ansible roles to install postgreses
+# User with passwordless ssh to targeted hosts is required
 
 if [ "$#" -ne 4 ]; then
-    echo "ERROR! Need Parameters: 1 - Postgres version, 2 - Master's name, 3 - Slave's name, 4 - Ansible inventory file" 
+    echo "ERROR! Need Parameters: 1 - Postgres version, 2 - Master's name, 3 - Slave's name, 4 - Ansible inventory file, 5 - Ansible user" 
     exit 1
 fi
 
@@ -14,6 +14,8 @@ master=$2
 slave=$3
 # Ansible Inventory file
 inventory=$4
+# Ansible user with root access
+ansible_user=$5
 
 echo Install Master
 ansible-playbook -i $inventory -l $master postgres_main.yml -e "postgresql_version=$pg_ver"
@@ -21,16 +23,16 @@ echo Install Slave
 ansible-playbook -i $inventory -l $slave postgres_main.yml --tags slave -e "postgresql_version=$pg_ver"
 
 echo Edit repmgr.conf
-ssh ansible@$master sudo sed -i 's/^#node_id=1/node_id=1/g' /etc/repmgr/$pg_ver/repmgr.conf
-ssh ansible@$slave sudo sed -i 's/^#node_id=1/node_id=2/g' /etc/repmgr/$pg_ver/repmgr.conf
+ssh $ansible_user@$master sudo sed -i 's/^#node_id=1/node_id=1/g' /etc/repmgr/$pg_ver/repmgr.conf
+ssh $ansible_user@$slave sudo sed -i 's/^#node_id=1/node_id=2/g' /etc/repmgr/$pg_ver/repmgr.conf
 
 echo Register Master
-ssh ansible@$master sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr primary register
+ssh $ansible_user@$master sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr primary register
 echo Clone Slave
-ssh ansible@$slave sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr -h $master -U repmgr -d repmgr standby clone
+ssh $ansible_user@$slave sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr -h $master -U repmgr -d repmgr standby clone
 echo Start Slave
 ansible -i $inventory $slave -a "/usr/pgsql-$pg_ver/bin/pg_ctl start -D /var/lib/pgsql/$pg_ver/data" --become --become-user=postgres -u ansible 
 echo Register Slave
-ssh ansible@$slave sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr standby register
+ssh $ansible_user@$slave sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr standby register
 
-ssh ansible@$slave sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr cluster show
+ssh $ansible_user@$slave sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr cluster show
