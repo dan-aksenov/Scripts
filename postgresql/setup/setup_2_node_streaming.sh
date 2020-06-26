eb21a4ee89695179610b18e61ae47e341467e89b
dbax <dan-aksenov@yandex.ru>
2020-06-26 11:44:28 +0300

    added pause between steps

--- postgresql/setup/setup_2_node_streaming.sh
+++ postgresql/setup/setup_2_node_streaming.sh
@@ -19,20 +19,24 @@ ansible_user=$USER
 
 echo Install Master
 ansible-playbook -i $inventory -l $master postgres_main.yml -e "postgresql_version=$pg_ver"
+read -p "Press [Enter] key to proceed..."
 echo Install Slave
 ansible-playbook -i $inventory -l $slave postgres_main.yml --tags slave -e "postgresql_version=$pg_ver"
-
+read -p "Press [Enter] key to proceed..."
 echo Edit repmgr.conf
 ssh $ansible_user@$master sudo sed -i 's/^#node_id=1/node_id=1/g' /etc/repmgr/$pg_ver/repmgr.conf
 ssh $ansible_user@$slave sudo sed -i 's/^#node_id=1/node_id=2/g' /etc/repmgr/$pg_ver/repmgr.conf
-
+read -p "Press [Enter] key to proceed..."
 echo Register Master
 ssh $ansible_user@$master sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr primary register
+read -p "Press [Enter] key to proceed..."
 echo Clone Slave
 ssh $ansible_user@$slave sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr -h $master -U repmgr -d repmgr standby clone
+read -p "Press [Enter] key to proceed..."
 echo Start Slave
 ansible -i $inventory $slave -a "/usr/pgsql-$pg_ver/bin/pg_ctl start -D /var/lib/pgsql/$pg_ver/data" --become --become-user=postgres -u ansible 
+read -p "Press [Enter] key to proceed..."
 echo Register Slave
 ssh $ansible_user@$slave sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr standby register
-
+read -p "Press [Enter] key to proceed..."
 ssh $ansible_user@$slave sudo -iu postgres /usr/pgsql-$pg_ver/bin/repmgr cluster show
