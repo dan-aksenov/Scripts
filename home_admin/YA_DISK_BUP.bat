2b3b7b334cec64c3ba7be56e9a79a98161e2eaab
dan-aksenov <dan-aksenov@yandex.ru>
2023-02-28 12:58:47 +0300

    more dirs excluded

--- home_admin/YA_DISK_BUP.bat
+++ home_admin/YA_DISK_BUP.bat
@@ -1,3 +1,3 @@
 chcp 1251
-robocopy E:\Users\Danila\YandexDisk\ \\Wdmycloud\danila\!BUPS\ya *.* /s /MIR /PURGE /xd !BUPS .sync
+robocopy E:\Users\Danila\YandexDisk\ \\Wdmycloud\danila\!BUPS\ya *.* /s /MIR /PURGE /xd !BUPS .sync video_share
 pause
