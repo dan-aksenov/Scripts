7b2cf79322cdd91cd3033f0e00eced7939524b43
dbax <dan-aksenov@yandex.ru>
2024-10-14 22:19:00 +0300

    path fixed

--- home_admin/PHOTOS_to_CLOUDS_BUP.bat
+++ home_admin/PHOTOS_to_CLOUDS_BUP.bat
@@ -1,14 +1,14 @@
 chcp 1251
 
 rem logging directory
-set log_dir=E:\TMP
+set log_dir=D:\TMP
 IF NOT EXIST %log_dir% MD %log_dir%
 
 rem set source
 set src="F:\Users\danila\Pictures\Мои фотографии"
 
 rem set YandexDisk destination
-set dst="E:\Users\danila\YandexDisk\!BUPS\fotos\Мои фотографии"
+set dst="D:\Users\danila\YandexDisk\!BUPS\fotos\Мои фотографии"
 set log=/unilog:"%log_dir%\photo_to_Ya.log"
 
 robocopy %src% %dst% *.* %log% /tee /z /mir
